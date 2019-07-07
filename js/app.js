064da9f80baadb294e825d9d6cd2a14ec426d906
Elise Falgout <emfalgout@gmail.com>
2019-07-07 12:28:30 -0700

    Finished Game
    finished game minus final comments and README

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
  */
 
 let cards = document.querySelectorAll('.card');
+let cardNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -21,22 +23,42 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
-// Loop through each card and create its HTML
+/*
+ * Reset the board
+ */
 function reset() {
+  cardNum = shuffle(cardNum); // Shuffle card order
+  reorderCards(cardNum); // Reorder the cards
+  document.querySelector('.moves').textContent = '0'; // Reset the move counter
+
+   // Loop through all the cards and make them "face down"
   for (let i = 0; i < cards.length; i++) {
-    shuffle(cards);
     cards[i].classList.remove('match', 'open', 'show'); //removes match, open and show classes
-    document.querySelector('.moves').textContent = '0';
   }
 
+  card = document.querySelector('.deck'); // update the deck
+  openCards = document.querySelector('.open'); // update the open cards list
+  moves = document.querySelector('.moves'); // reset the counter
+  numMoves = 0; // make the number of moves 0
+  stars.firstElementChild.setAttribute('style', 'display: inline-block;'); // display first star
+  stars.lastElementChild.setAttribute('style', 'display: inline-block;'); // display last star
+  timer.setAttribute('style', 'padding-left: 25px; display: none;'); // hide timer
+  timer.textContent = '0s'; // reset initial stopwatch display
+  time = 0; // reset time second counter
+  numStars = 3; // reset number of stars
 }
 
-
-
+/*
+ * Reorders the cards to reflect the new order given by the shuffle function
+ */
+function reorderCards(cardNum) {
+  for (let i = 0; i < cardNum.length; i++) {
+    cards[cardNum[i]].style.order = i;
+  }
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -49,16 +71,30 @@ function reset() {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 let card = document.querySelector('.deck');
-card.addEventListener('click', respondToCardClick);
 let openCards = document.querySelector('.open');
-let numMoves = 0;
 let moves = document.querySelector('.moves');
+const resetButton = document.querySelector('.restart');
+let stars = document.querySelector('.stars');
+let numMoves = 0;
+card.addEventListener('click', respondToCardClick);
+resetButton.addEventListener('click', respondToResetClick);
+let time = 0;
+let end = false;
+let stopwatch;
+let timer = document.querySelector('.timer');
+let numStars = 3;
+let playAgainBttn = document.querySelector('.play-again');
+playAgainBttn.addEventListener('click', respondToPlayClick);
+reset();
 
 
 function respondToCardClick(evt) {
   if (evt.target.nodeName === 'LI' && evt.target.className === 'card') {
+    if (numMoves === 0) {
+      stopwatch = setInterval(getTotalTime, 1000);
+      timer.setAttribute('style', 'padding-left: 25px; display: inline-block;');
+    }
     evt.target.classList.add('open', 'show');
-
     /*
      * Checks to see if a card is open or not.
      * If there are no cards open, set the clicked card to openCard
@@ -71,9 +107,10 @@ function respondToCardClick(evt) {
      */
     if (openCards == null){
      openCards = evt.target;
+     moveCounter();
 
    } else {
-     if (openCards.isEqualNode(evt.target)){
+     if (openCards.firstElementChild.isEqualNode(evt.target.firstElementChild)){
        matched(evt.target, openCards);
      }
      else {
@@ -81,27 +118,86 @@ function respondToCardClick(evt) {
      }
      openCards = null;
    }
-    moveCounter();
   }
 }
 
 function matched(card1, card2) {
   card1.classList.toggle('match', 'open', 'show');
   card2.classList.toggle('match', 'open', 'show');
+  card1.classList.add('boing');
+  card2.classList.add('boing');
+  animationEnd(card1, 'boing');
+  animationEnd(card2, 'boing');
+  moveCounter();
+  gameFinished();
 }
 
 function noMatch(card1, card2){
-  card1.classList.remove('open', 'show');
-  card2.classList.remove('open', 'show');
+  card1.classList.add('shake');
+  card2.classList.add('shake');
+  animationEnd(card1, 'shake');
+  animationEnd(card2, 'shake');
+  moveCounter();
 }
 
 function moveCounter(){
   numMoves++;
   moves.textContent = numMoves;
-  console.log('Number of moves: ' + numMoves);
+  starTracker(numMoves);
 }
 
 function gameFinished(){
   let matchedCards = document.querySelectorAll('.match');
+  if (matchedCards.length == cards.length) {
+    let endingTime = performance.now();
+    console.log(endingTime);
+    end = true;
+    let totalTime = getTotalTime();
+    clearInterval(stopwatch);
+    document.querySelector('.modal').style.display = "flex";
+    document.querySelector('.game-stats').textContent = numMoves + " moves in " + totalTime + " with " + numStars + " stars!";
+  }
+}
 
+function respondToResetClick(e){
+  clearInterval(stopwatch);
+  reset();
+}
+
+function animationEnd(animated, animateClass){
+  animated.addEventListener('animationend', () => {
+    if (animateClass === 'shake') {
+    animated.classList.remove(animateClass, 'open', 'show');
+  } else {
+    animated.classList.remove(animateClass);
+  }
+  });
+}
+
+function starTracker(num){
+  if (num == 22) {
+    stars.firstElementChild.setAttribute('style', 'display: none;');
+    numStars--;
+  } else if (num == 38) {
+    stars.lastElementChild.setAttribute('style', 'display: none');
+    numStars--;
+  }
+}
+
+function getTotalTime(){
+  time++;
+  if (time < 60){
+    timer.textContent = time + 's';
+    return time + 's';
+  } else if (time >= 60){
+    let minTime = Math.floor(time / 60);
+    let secTime = time - (minTime * 60);
+    timer.textContent = minTime + 'm ' + secTime + 's';
+    return minTime + 'm ' + secTime.toFixed(0) + 's';
+  }
+}
+
+function respondToPlayClick(){
+  document.querySelector('.modal').style.display = "none";
+  reset();
 }
